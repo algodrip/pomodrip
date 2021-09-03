d2ba4f0aa4e765a6c9a8ab555a788e78a014653e
Neoloopy <46285089+Neoloopy@users.noreply.github.com>
2021-09-03 12:22:16 +0800

    Update main.py
    fixed a minor bug
    - When starting the timer for the first time, the input display is "00," however, once used, the timer input display is "0"
    
    added new comments but did not chance the format of the original (task assigned to @izayaaaaaaa)
    fixed 1 comment as it was in reverse
    "minutes to hours" to "hours to minutes"
    fixed the format of some code

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ minute.set("00")
 second.set("00")
 
 #Input for each variable
-hour_entry = Entry(root, font = ("Arial",12), textvariable = hour, width=5)
-hour_entry.place(x=80,y=20)
+hour_entry = Entry(root, font = ("Arial", 12), textvariable = hour, width = 5)
+hour_entry.place(x = 80, y = 20)
 
-minute_entry = Entry(root, font = ("Arial",12), textvariable = minute, width=5)
-minute_entry.place(x=130,y=20)
+minute_entry = Entry(root, font = ("Arial", 12), textvariable = minute, width = 5)
+minute_entry.place(x = 130, y = 20)
 
-second_entry = Entry(root, font = ("Arial",12), textvariable = second, width=5)
-second_entry.place(x=180,y=20)
+second_entry = Entry(root, font = ("Arial", 12), textvariable = second, width = 5)
+second_entry.place(x = 180, y = 20)
 
 #Converts seconds into hour, minute, and second
 def TimeInput():
@@ -40,7 +40,7 @@ def TimeInput():
 		#converts minutes to seconds
 		mins, secs = divmod(Timing,60)
 
-		#converts minutes to hours
+		#converts hours to minutes 
 		hours = 0
 		if mins > 60:
 			hours, mins = divmod(mins,60)
@@ -56,11 +56,16 @@ def TimeInput():
 		#time's up display
 		if Timing == 0:
 			messagebox.showinfo("Timer", "Time's up! 🎊")
+			# sets the timer back to 00 instead of 0
+			hour.set("00")
+			minute.set("00")
+			second.set("00")
 		#subtracts the time
 		Timing -= 1
 
 #Activation button
-Button_Entry = Button(root, text = "Start!", bd = "5", command = TimeInput, width=20, compound="c")
+Button_Entry = Button(root, text = "Start!", bd = "5", command = TimeInput, width = 20, compound = "c")
 Button_Entry.place(x = 70, y = 120)
 
+# loops the window to keep it active
 root.mainloop()
