c7487ab1c77629bba71b7f5424a0f926d31663fc
angelofallars <ba.fallaria@gmail.com>
2021-09-06 02:20:30 +0800

    Refactor foreground and background values into own constants

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ root.resizable(width=False, height=False)
 
 # Constants
 FONT = "Arial"
+FOREGROUND = "#C72C41"
+BACKGROUND = "#510A32"
 
 # --- Timer ---
 
@@ -34,19 +36,19 @@ hour_text = tk.Label(root, font=(FONT, 12), fg="#EE4540")
 # Input for each variable
 hour_entry = tk.Entry(root, font=(FONT, 24),
                       textvariable=hour, width=5,
-                      fg="#C72C41", bg="#510A32",
+                      fg=FOREGROUND, bg=BACKGROUND,
                       justify="center", bd="0")
 hour_entry.place(x=10, y=30)
 
 minute_entry = tk.Entry(root, font=(FONT, 24),
                         textvariable=minute, width=5,
-                        fg="#C72C41", bg="#510A32",
+                        fg=FOREGROUND, bg=BACKGROUND,
                         justify="center", bd="0")
 minute_entry.place(x=100, y=30)
 
 second_entry = tk.Entry(root, font=(FONT, 24),
                         textvariable=second, width=5,
-                        fg="#C72C41", bg="#510A32",
+                        fg=FOREGROUND, bg=BACKGROUND,
                         justify="center", bd="0")
 second_entry.place(x=190, y=30)
 
@@ -98,7 +100,7 @@ def time_input():
 button_entry = tk.Button(root, text="Start!", bd="0",
                          command=time_input, width=38,
                          compound="c",
-                         fg="#C72C41", bg="#510A32")
+                         fg=FOREGROUND, bg=BACKGROUND)
 button_entry.place(x=10, y=75)
 
 
@@ -132,8 +134,8 @@ todo_list_frame.place(x=300, y=30)
 # Create the actual to do list
 todo_list = tk.Listbox(todo_list_frame, width=25,
                        height=7, font=(FONT, 12),
-                       bd=0, fg="#C72C41",
-                       bg="#510A32", activestyle="none") 
+                       bd=0, fg=FOREGROUND,
+                       bg=BACKGROUND, activestyle="none") 
 todo_list.pack(side=tk.LEFT, fill=tk.BOTH)
 
 # task_list values
@@ -153,7 +155,7 @@ todo_list_scroll_bar.config(command=todo_list.yview)
 
 # Create an entry box for the to do list
 todo_list_entry = tk.Entry(root, font=(FONT, 12),
-                           fg="#C72C41", bg="#510A32",
+                           fg=FOREGROUND, bg=BACKGROUND,
                            bd=1, width=26)
 todo_list_entry.place(x=300, y=170)
 
@@ -179,14 +181,14 @@ def del_task():
 # Insert text into the listbox
 addtask_button = tk.Button(list_button_frame, text="Insert",
                            font=(FONT, 12), bd=0,
-                           fg="#C72C41", bg="#510A32",
+                           fg=FOREGROUND, bg=BACKGROUND,
                            width=10, command=new_task)
 addtask_button.pack(fill=tk.BOTH, expand=True, side=tk.TOP)
 
 # Delete items in the list
 del_task_button = tk.Button(list_button_frame, text="Remove",
                             font=(FONT, 12), bd=0,
-                            fg="#C72C41", bg="#510A32",
+                            fg=FOREGROUND, bg=BACKGROUND,
                             width=10, command=del_task)
 del_task_button.pack(fill=tk.BOTH, expand=True, side=tk.BOTTOM)
 
