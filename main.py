87ddf83972252e8d5a1d67becf1600a18be370d2
Neoloopy <46285089+Neoloopy@users.noreply.github.com>
2021-09-05 18:37:37 +0800

    GUI redesign and to-do list
    Changing the layout of the GUI (Work In progress)
    
    New feature!
    -  To-do list
    - Entry to let people add tasks
    -  Added a to do list insert and delete button

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ root = tk.Tk()
 # title
 root.title("PomoDrip")
 # window size
-root.geometry("300x200")
+root.geometry("700x200")
+# window background color
+root.configure(background="#120130")
+#disables resizing of the window
+root.resizable(width=False, height=False)
 
 # Variables
 hour = tk.StringVar()
@@ -20,14 +24,23 @@ minute.set("00")
 second.set("00")
 
 # Input for each variable
-hour_entry = tk.Entry(root, font = ("Arial", 12), textvariable = hour, width = 5)
-hour_entry.place(x = 80, y = 20)
+hour_entry = tk.Entry(root, font = ("Arial", 24)
+		, textvariable = hour, width = 5
+		, fg = "#059fb0", bg = "#1b0248"
+		, justify="center", bd = "0")
+hour_entry.place(x = 10, y = 30)
 
-minute_entry = tk.Entry(root, font = ("Arial", 12), textvariable = minute, width = 5)
-minute_entry.place(x = 130, y = 20)
+minute_entry = tk.Entry(root, font = ("Arial", 24)
+		, textvariable = minute, width = 5
+		, fg = "#059fb0", bg = "#1b0248"
+		, justify="center", bd = "0")
+minute_entry.place(x = 100, y = 30)
 
-second_entry = tk.Entry(root, font = ("Arial", 12), textvariable = second, width = 5)
-second_entry.place(x = 180, y = 20)
+second_entry = tk.Entry(root, font = ("Arial", 24)
+		, textvariable = second, width = 5
+		, fg = "#059fb0", bg = "#1b0248"
+		, justify="center", bd = "0")
+second_entry.place(x = 190, y = 30)
 
 
 # Converts seconds into hour, minute, and second
@@ -44,7 +57,8 @@ def time_input():
 			while Timing > -1:
 				# converts minutes to seconds
 				mins, secs = divmod(Timing, 60)
-
+				# resizes the window
+				root.geometry("300x200")
 				# converts hours to minutes
 				hours = 0
 				if mins > 60:
@@ -66,12 +80,17 @@ def time_input():
 					hour.set("00")
 					minute.set("00")
 					second.set("00")
+					# resets the window size
+					root.geometry("700x200")
 				# subtracts the time
 				Timing -= 1
 
 # Activation button
-Button_Entry = tk.Button(root, text = "Start!", bd = "5", command = time_input, width = 20, compound = "c")
-Button_Entry.place(x = 70, y = 120)
+Button_Entry = tk.Button(root, text = "Start!", bd = "0"
+		, command = time_input, width = 38
+		, compound = "c"
+		, fg = "#FD9A24", bg = "#1b0248")
+Button_Entry.place(x = 10, y = 75)
 
 # Defines timer_entry_clear function
 def second_entry_clear(en):
@@ -90,6 +109,72 @@ def hour_entry_clear(en):
 hour_entry.bind("<Button-1>", hour_entry_clear)
 minute_entry.bind("<Button-1>", minute_entry_clear)
 second_entry.bind("<Button-1>", second_entry_clear)
+
+# frame used to separate the to do list
+to_do_list_frame = tk.Frame(root)
+to_do_list_frame.place(x = 400, y = 30)
+
+# creating the actual to do list
+To_do_list = tk.Listbox(to_do_list_frame, width = 25
+		, height = 8, font = ("Arial", 12)
+		, bd = 0, fg = "#059fb0"
+		, bg = "#1b0248", activestyle = "none") 
+To_do_list.pack(side = tk.LEFT, fill = tk.BOTH)
+
+# task_list values
+task_list = [
+	"do school work",
+	"finish this project",
+	"anime",
+]
+
+# inserting a new task
+for item in task_list:
+	To_do_list.insert(tk.END, item)
+
+# scroll bar for to do list
+to_do_list_scroll_bar = tk.Scrollbar(to_do_list_frame)
+to_do_list_scroll_bar.pack(side = tk.RIGHT, fill = tk.BOTH)
+
+# controls for the scroll bar
+To_do_list.config(yscrollcommand = to_do_list_scroll_bar.set)
+to_do_list_scroll_bar.config(command = To_do_list.yview)
+
+# creates an entry box for the to do list
+to_do_list_entry = tk.Entry(root, font = ("Arial", 12)
+		, fg = "#059fb0", bg = "#1b0248"
+		, bd = 0)
+to_do_list_entry.place(x = 10, y = 110)
+
+# creates a frame for the list buttons
+list_button_frame = tk.Frame(root)
+list_button_frame.place(x = 10, y = 140)
+
+# defines new task
+def new_task():
+	task = to_do_list_entry.get()
+	if task != "": 
+		To_do_list.insert(tk.END, task)
+	else:
+		messagebox("Error", "Please enter some task.")
+
+# defines delete task
+def del_task():
+    To_do_list.delete(tk.ANCHOR) 
+
+# inserts text into the listbox
+addtask_button = tk.Button(list_button_frame, text = "Insert"
+		, font = ("Arial", 12), bd = 0
+		, fg = "#059fb0", bg = "#1b0248"
+		, command = new_task)
+addtask_button.pack(fill = tk.BOTH, expand = True, side = tk.LEFT)
+
+# deletes items in the list 
+del_task_button = tk.Button(list_button_frame, text = "Remove"
+		, font = ("Arial", 12), bd = 0
+		, fg = "#059fb0", bg = "#1b0248"
+		, command = del_task)
+del_task_button.pack(fill = tk.BOTH, expand = True, side = tk.LEFT)
 
 # loops the window to keep it active
 root.mainloop()
