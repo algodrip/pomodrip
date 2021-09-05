e9d174126cfdca0eb32da9c2eaffa5d733c29b33
Neoloopy <46285089+Neoloopy@users.noreply.github.com>
2021-09-05 12:48:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,39 +9,30 @@ root.title("PomoDrip")
 # window size
 root.geometry("300x200")
 # window background color
-root.configure(background="#010114")
+root.configure(background="#4C4C4C")
 
 ''' Experimental code (testing out how to create a custom title and menu bar)
-
 # Defines drag movement
 def drag_movement(drag):
 	root.geometry(f'+{drag.x_root}+{drag.y_root}')
-
 # remove title bar
 root.overrideredirect(True)
-
 # creates fake title bar
 title_bar = tk.Frame(root, bg = "#010114", relief = "raised", bd = 0)
 title_bar.pack(side = tk.TOP, expand = 1, fill = tk.BOTH)
-
 #title text
 title_label = tk.Label(title_bar, text = " PomoDrip", font = ("Arial", 10), bg = "#010114", fg = "#ffa500")
 title_label.place(x = 0, y = 0)
-
 #creates title bar close button
 title_button_close = tk.Button(title_bar, text = "  x  ", font = ("Arial", 12), bd = 0, bg = "#010114", fg = "#ffa500", command = root.destroy)
-
 # places title bar close button
 title_button_close.place(x = 260, y = 0)
-
 !!!WARNING!!! This line of code does not function while root.overridedirect is set to True
 #creates a minimize button
 title_button_minimize = tk.Button(title_bar, text = "  _  ", font = ("Arial", 12), bd = 0, bg = "#010114", fg = "#ffa500", command = lambda: root.wm_state("iconic"))
 # places title bar minimize button
 title_button_minimize.place(x = 230, y = 0)
 !!!WARNING!!!
-
-
 #Binds the title bar (allows it to be dragged)
 title_bar.bind("<B1-Motion>", drag_movement)
 title_label.bind("<B1-Motion>", drag_movement)
@@ -58,13 +49,13 @@ minute.set("00")
 second.set("00")
 
 # Input for each variable
-hour_entry = tk.Entry(root, font = ("Arial", 12), textvariable = hour, width = 5, bg = "#010114", fg = "#ffa500")
+hour_entry = tk.Entry(root, font = ("Arial", 12), textvariable = hour, width = 5, bg = "#4C4C4C", fg = "#ffa500")
 hour_entry.place(x = 80, y = 40)
 
-minute_entry = tk.Entry(root, font = ("Arial", 12), textvariable = minute, width = 5, bg = "#010114", fg = "#ffa500")
+minute_entry = tk.Entry(root, font = ("Arial", 12), textvariable = minute, width = 5, bg = "#4C4C4C", fg = "#ffa500")
 minute_entry.place(x = 130, y = 40)
 
-second_entry = tk.Entry(root, font = ("Arial", 12), textvariable = second, width = 5, bg = "#010114", fg = "#ffa500")
+second_entry = tk.Entry(root, font = ("Arial", 12), textvariable = second, width = 5, bg = "#4C4C4C", fg = "#ffa500")
 second_entry.place(x = 180, y = 40)
 
 
